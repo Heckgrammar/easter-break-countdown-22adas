db944494a6b4aa0d6efd72cfe56ef831a0e591cc
Aayush Das <22ADas@heckgrammar.co.uk>
2025-04-04 10:15:18 +0100

    YIPPPEEE

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -4,6 +4,19 @@
     {
         static void Main(string[] args)
         {
+            DateTime current = DateTime.Now;
+            DateTime target = new DateTime(2025,4,4,15,15,0);
+
+            TimeSpan difference = current - target;
+            Console.WriteLine(difference);
+
+            while (true)
+            {
+                DateTime current2 = DateTime.Now;
+                Console.WriteLine(current2);
+                Thread.Sleep(1000);
+            }
+
             
         }
     }
